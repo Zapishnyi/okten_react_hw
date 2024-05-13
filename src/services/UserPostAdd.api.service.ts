fc130b5f7e9b9bfcc1818f5d9ca63209ec9689cc
Zapishnyi <zapishny@ukr.net>
2024-05-13 23:32:11 +0300

    api service add

--- src/services/UserPostAdd.api.service.ts
+++ src/services/UserPostAdd.api.service.ts
@@ -1,6 +1,16 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+import { IPostProps } from "../models/IPostProps";
 
-let AxiosInstance: AxiosInstance = axios.create({
+let axiosInstance: AxiosInstance = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
   headers: { "Content-Type": "application/json" },
 });
+
+const postUserPost = (props: IPostProps) =>
+  axiosInstance.post("/post", {
+    title: props.title,
+    body: props.body,
+    userId: props.userId,
+  });
+
+export { postUserPost };
