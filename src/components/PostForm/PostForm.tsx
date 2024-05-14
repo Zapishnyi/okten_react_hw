83b852edf9f78069bd8b430753e5a877b3537751
Zapishnyi <zapishny@ukr.net>
2024-05-14 09:02:23 +0300

    useForm built

--- src/components/PostForm/PostForm.tsx
+++ src/components/PostForm/PostForm.tsx
@@ -1,25 +1,31 @@
 import React from "react";
 import styles from "./PostForm.module.css";
+import { useForm } from "react-hook-form";
+import { IPostProps } from "../../models/IPostProps";
 
 const PostForm = () => {
+  let postForm = useForm<IPostProps>();
+  let { register, handleSubmit } = postForm;
+  console.log(postForm);
+  // handleSubmit(() => {});
+  const handleSubmitCallback = (formData: IPostProps) => {};
+
   return (
-    <div>
-      <form className={styles.form} action="">
-        <label>
-          Post title:
-          <input type="text" />
-        </label>
-        <label>
-          Post body:
-          <input type="text" />
-        </label>
-        <label>
-          User ID:
-          <input type="text" />
-        </label>
-        <button>Submit</button>
-      </form>
-    </div>
+    <form className={styles.form} onSubmit={handleSubmit(handleSubmitCallback)}>
+      <label>
+        User ID:
+        <input type="number" {...register("userId")} />
+      </label>
+      <label>
+        Post title:
+        <input type="text" {...register("title")} />
+      </label>
+      <label>
+        Post body:
+        <textarea {...register("body")} />
+      </label>
+      <button>Submit</button>
+    </form>
   );
 };
 
